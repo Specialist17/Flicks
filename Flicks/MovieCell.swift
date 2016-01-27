f64031fe5328cdf2f36610b52f7b2f19c1bad207
Specialist17 <Specialist@Fernandos-MacBook-Pro-5.local>
2016-01-27 17:24:28 -0400

    first commit

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Fernando on 1/27/16.
+//  Copyright © 2016 Fernando. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
